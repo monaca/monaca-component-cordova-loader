2f156a41ef9486f6ce6222a5c2ef51a449911244
HIROSE Masaya <hirose@asial.co.jp>
2020-10-05 22:03:12 +0900

    Updated for Cordova10.

--- cordova-loader.js
+++ cordova-loader.js
@@ -3,7 +3,7 @@
     var raw_values = window.location.search.substring(1).split('&');
     var values = {};
     var device = { platform: "" };
-    
+
     if (raw_values) {
       for (var key in raw_values) {
         var tmp = raw_values[key].split('=');
@@ -11,21 +11,30 @@
       }
       device.platform = values.platform;
     }
-    
+
     return device;
   }
-    
+
   if (location && typeof location.href === "string" && /^https:\/\/preview-.+monaca\.(local||mobi)/.test(location.href)) {
     window.device = getDeviceObjectForPreview();
   }
 
-  if ((navigator.userAgent.match(/Android/i)) || (navigator.userAgent.match(/iPhone|iPad|iPod/i))) {
+  if (
+      (navigator.userAgent.match(/Android/i))
+      || (navigator.userAgent.match(/iPhone|iPad|iPod/i))
+      || (navigator.userAgent.match(/Macintosh; Intel Mac OS X/i) && location.protocol.match(/^https?:/) === null)
+  ) {
+
     if (typeof location.href === "string") {
-      var relativePath = location.href.split("/www")[1];
-      var paths = relativePath.split("/");
-      var cordovaJsUrl = ""; 
-      for (var i = 0; i < paths.length - 2; i++) {
-        cordovaJsUrl += "../";
+      var cordovaJsUrl = location.protocol + "//" + location.hostname + "/";
+      var relativePath = "";
+      if (location.href.indexOf('/www') !== -1) {
+        relativePath = location.href.split("/www")[1];
+        var paths = relativePath.split("/");
+        cordovaJsUrl = "";
+        for (var i = 0; i < paths.length - 2; i++) {
+          cordovaJsUrl += "../";
+        }
       }
       document.write("<script src=\"" + cordovaJsUrl+ "cordova.js" + "\"></script>");
     }
